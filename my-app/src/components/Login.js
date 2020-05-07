611bb308d668d934d0dd1d2599320b9a9eed1f50
Simone Roy <simonecarolynr@gmail.com>
2020-05-06 18:31:40 -0600

    login component

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,13 +1,34 @@
 import React, { useState } from 'react';
 
-export const Login = () => {
+export const Login = (props) => {
 
-    const [ isLoading ] = useState(false)
+    const [ isLoading, setIsLoading ] = useState(false)
+    const [ user, setUser ] = useState({ 
+        username: 'Lambda School', 
+        password: 'i<3Lambd4' 
+    });
+
+    const hanldeSubmit = e => {
+        axios
+            .post('/api/login', user)
+            .then(res => {
+                setIsLoading(true);
+                localStorage.setItem('token', res.data.token);
+                props.history.push('/friends');
+            })
+            .catch(err => console.log(err))
+    }
+
+    const handleChange = e => {
+        setUser({
+            [e.target.name] : e.target.value
+        })
+    }
 
     return (
-        <form>
-            <input name='username' placeholder='Username' />
-            <input name='password' type='password' placeholder='Password' />
+        <form onSubmit={hanldeSubmit}>
+            <input name='username' placeholder='Username' onChange={handleChange} />
+            <input name='password' type='password' placeholder='Password' onChange={handleChange}/>
             <button type='submit' />
         </form>
     )
